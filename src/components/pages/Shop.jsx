20cfa42b2212a6c8def2a703f67c6e0f2789bda4
Anuragchindaliya <ak1148383@gmail.com>
2021-11-28 12:45:14 +0530

    Add: sorting of products

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -6,30 +6,48 @@ import _ from "lodash";
 import ShopSkeleton from "components/common/skeleton/shopSkeleton";
 
 function Shop({ products: { data: products, loading }, categories, searchActions }) {
-  const [allproducts, setAllProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const { slug } = useParams();
-  const catSlug = slug ? slug.toLowerCase() : "";
   const { searchProductsReset } = searchActions;
 
-  const finalProducts =
-    allproducts.length > 0
-      ? catSlug
-        ? allproducts.filter((pr) => {
-          var ans = false;
-          pr.categories.forEach((ele) => {
-            if (ele.slug === catSlug) {
-              ans = true;
-            }
-          });
-          return ans;
-        })
-        : products
-      : [];
+
+  const handleSorting = (event) => {
+    const sortedProducts = [...allProducts];
+
+    setAllProducts(sortedProducts.sort((p1, p2) => {
+      switch (event.target.value) {
+        case 'priceLowtoHigh':
+          return parseInt(p1.prices.sale_price) - parseInt(p2.prices.sale_price);
+        case 'priceHightoLow':
+          return parseInt(p2.prices.sale_price) - parseInt(p1.prices.sale_price);
+        // case 'newestFirst':
+        //   return new Date(p2);
+        default:
+          return null;
+      }
+    }));
+    // setAllProducts(sortedProducts.sort((p1, p2)=>parseInt(p1.prices.sale_price)-parseInt(p2.prices.sale_price)));
+    // console.log('sorting', allProducts);
+  }
   useEffect(() => {
     if (!_.isEmpty(products)) {
       setAllProducts(products);
     }
   }, [products]);
+
+  useEffect(() => {
+    if (!_.isEmpty(slug) && !_.isEmpty(products)) {
+      const catSlug = slug ? slug.toLowerCase() : "";
+
+      const finalProducts = products.filter(pr => {
+        var ans = false;
+        pr.categories.forEach(ele => (ele.slug === catSlug) ? ans = true : null);
+        return ans;
+      });
+
+      setAllProducts(finalProducts);
+    }
+  }, [slug, products]);
 
   useEffect(() => {
     return () => searchProductsReset()
@@ -61,8 +79,11 @@ function Shop({ products: { data: products, loading }, categories, searchActions
                     <div className="row">
                       <div className="col-lg-7 col-md-7">
                         <div className="select-option">
-                          <select className="sorting">
+                          <select className="sorting" onChange={handleSorting}>
                             <option value>Default Sorting</option>
+                            <option value='priceLowtoHigh'>Price Low to High</option>
+                            <option value='priceHightoLow'>Price High to Low</option>
+                            <option value='newestFirst'>Newest first</option>
                           </select>
                           <select className="p-show">
                             <option value>Show:</option>
@@ -71,20 +92,20 @@ function Shop({ products: { data: products, loading }, categories, searchActions
                       </div>
                       <div className="col-lg-5 col-md-5 text-right">
                         {/* {finalProducts.length > 0 ? <p>Show 01- 09 Of {finalProducts.length} Product</p> : "No result"} */}
-                        {finalProducts.length > 0 ? <p>Show 01 - {finalProducts.length < 10 ? `0${finalProducts.length}` : finalProducts.length} Product</p> : "No result"}
+                        {allProducts.length > 0 ? <p>Show 01 - {allProducts.length < 10 ? `0${allProducts.length}` : allProducts.length} Product</p> : "No result"}
                       </div>
 
                     </div>
                   </div>
                   <div className="product-list">
                     <div className="row">
-                      {finalProducts.length > 0 ? finalProducts.map((el, index) => {
+                      {allProducts.length > 0 ? allProducts.map((el, index) => {
                         return (
                           <div key={index} className="col-lg-4 col-sm-6">
                             <ProductCard product={el} />
                           </div>
                         );
-                      }) : <><img src={"assets/img/cat.png"} style={{ margin: "auto" }} /></>
+                      }) : <><img alt='' src={"assets/img/cat.png"} style={{ margin: "auto" }} /></>
                       }
                     </div>
                   </div>
